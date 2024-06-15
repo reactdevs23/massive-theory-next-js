3da059ad516e5468da7690c2275121963b400958
dalimkumar604 <dalimkumar212582@gmail.com>
2024-06-15 18:01:24 +0600

    scrollingIssue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,14 @@
-"use client";
-
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
-import Head from "next/head";
 
+import Head from "next/head";
+import { Metadata } from "next";
+
+export const metadata = {
+  title: "...",
+  description: "...",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-lt-installed={true}>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { IoMdClose } from "react-icons/io";
 import { AiOutlineAlignRight } from "react-icons/ai";
